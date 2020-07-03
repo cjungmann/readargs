76fb4096add219903730ca92c442d9a080383bc2
Chuck Jungmann <chuckj60@gmail.com>
2020-07-03 07:13:30 -0500

    Added warning message generator and using it for int_reader() when the option value is not a number.

--- agents.c
+++ agents.c
@@ -1,8 +1,39 @@
 #include "readargs.h"
 
-#include <stdlib.h>   // for atoi
+#include <stdlib.h>   // for atoi()
+#include <string.h>   // for strchr()
 
-void ra_error_missing_target(const char *target_name)
+int str_is_number(const char *str)
+{
+   static const char numlist[] = "-0123456789";
+   const char *ptr = strchr(numlist, *str);
+   if (ptr && *ptr == '-')
+      ptr = strchr(&numlist[1], *(str+1));
+
+   return ptr != NULL;
+}
+
+EXPORT void ra_warn_invalid_input(FILE *f, const raOpt *opt, const char *msg)
+{
+   if (opt->letter > 0 || opt->label)
+   {
+      fprintf(f, "%s: option ", g_command_name);
+      if (opt->letter > 0)
+      {
+         fprintf(f, "-%c", opt->letter);
+         if (opt->label)
+            fprintf(f, "/--%s", opt->label);
+      }
+      else if (opt->label)
+         fprintf(f, "--%s", opt->label);
+
+      fputc(' ', f);
+      fputs(msg, f);
+      fputc('\n', f);
+   }
+}
+
+void warn_missing_target(const char *target_name)
 {
    fprintf(stderr, "%s: agent requires cached target", g_command_name);
    if (target_name)
@@ -16,7 +47,7 @@ int confirm_option_target(const raOpt *opt, const char *target_name)
       return 1;
    else
    {
-      ra_error_missing_target(target_name);
+      warn_missing_target(target_name);
       return 0;
    }
 }
@@ -26,7 +57,12 @@ int confirm_option_target(const raOpt *opt, const char *target_name)
 void int_reader(const raOpt *opt, const char *str)
 {
    if (confirm_option_target(opt, "int agent"))
-      *(int*)opt->target = atoi(str);
+   {
+      if (str_is_number(str))
+         *(int*)opt->target = atoi(str);
+      else
+         ra_warn_invalid_input(stderr, opt, "requires a number");
+   }
 }
 
 void int_writer(FILE *f, const raOpt *opt)
