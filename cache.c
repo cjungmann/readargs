afee284be6ddb6b16f1f5ffa6f482ad98046ccbd
Chuck Jungmann <chuckj60@gmail.com>
2020-07-07 15:24:57 -0500

    Refactor to use g_scheme and raTour instead of raCache.

--- cache.c
+++ cache.c
@@ -2,67 +2,321 @@
 
 #include <string.h>
 
-EXPORT int ra_init_cache(raCache *cache,
-                         const char **start_arg,
+raScene g_scene;
+
+const raOpt *seek_raOpt_by_label(const char *str)
+{
+   const raOpt *ptr = g_scene.options;
+   while (ptr < g_scene.options_end)
+   {
+      if (strcmp(ptr->label, str) == 0)
+         return ptr;
+
+      ++ptr;
+   }
+
+   return NULL;
+}
+
+const raOpt *seek_raOpt_by_letter(char letter)
+{
+   const raOpt *ptr = g_scene.options;
+   while (ptr < g_scene.options_end)
+   {
+      if (ptr->letter == letter)
+         return ptr;
+
+      ++ptr;
+   }
+
+   return NULL;
+}
+
+int are_arguments_required(const raOpt *opt)
+{
+   return opt && opt->agent && opt->agent->args_needed;
+}
+
+EXPORT const char *ra_command_name(void)
+{
+   const char *str = strrchr(*g_scene.args,'/');
+   if (str)
+      return str+1;
+   else
+      return *g_scene.args;
+}
+
+EXPORT void ra_set_scene(const char **start_arg,
                          int arg_count,
                          const raOpt *start_opt,
                          int opt_count)
 {
-   cache->args = start_arg;
-   cache->args_end = start_arg + arg_count;
-   cache->options = start_opt;
-   cache->options_end = start_opt + opt_count;
-
-   cache->current_arg = start_arg;
-   cache->current_option = start_opt;
-   cache->last_position_option = NULL;
-   cache->sub_arg_ptr = NULL;
-
-   return 1;
+   g_scene.args = start_arg;
+   g_scene.args_end = start_arg + arg_count;
+   g_scene.options = start_opt;
+   g_scene.options_end = start_opt + opt_count;
 }
 
-EXPORT const char *ra_command_name(const raCache *cache)
+EXPORT raTour *ra_start_tour(raTour *tour)
 {
-   const char *str = strrchr(*cache->args,'/');
-   if (str)
-      return str+1;
-   else
-      return *cache->args;
+   tour->current_arg = g_scene.args;
+   tour->current_option = g_scene.options;
+
+   tour->sub_arg_ptr = NULL;
+   tour->last_position_option = NULL;
+
+   return tour;
 }
 
-EXPORT const char *ra_next_arg(raCache *cache)
+EXPORT const char* ra_advance_arg(raTour *tour)
 {
-   cache->sub_arg_ptr = NULL;
-   if (cache->current_arg + 1 < cache->args_end)
-   {
-      ++cache->current_arg;
-      return *cache->current_arg;
-   }
+   tour->sub_arg_ptr = NULL;
+   if (tour->current_arg < g_scene.args_end)
+      return *++tour->current_arg;
    else
       return NULL;
 }
 
+EXPORT raStatus ra_advance_option(raTour *tour, const raOpt **option, const char **value)
+{
+   *option = NULL;
+   *value = NULL;
+
+   const char **options_str = &tour->sub_arg_ptr;
+   const raOpt *opt = NULL;
+
+   if (! *options_str || ! **options_str)
+   {
+      const char *arg = ra_advance_arg(tour);
+
+      if (arg)
+      {
+         if (*arg == '-')
+         {
+            ++arg;
+            if (*arg == '-')
+            {
+               ++arg;
+               if (*arg)  // long option, double-dash + label
+               {
+                  opt = seek_raOpt_by_label(arg);
+                  if (opt)
+                  {
+                     *option = opt;
+                     return RA_SUCCESS;
+                  }
+               }
+               else       // options terminator, naked double-dash "--"
+                  return RA_END_OPTIONS;
+            }
+            else          // short option, single-dash + letter
+               *options_str = arg;
+         }
+      }
+   }
+      
+   if (*options_str)
+   {
+      char letter = **options_str;
+      ++*options_str;
+      opt = seek_raOpt_by_letter(letter);
+      if (opt)
+      {
+         *option = opt;
+         if (are_arguments_required(opt))
+         {
+            if (**options_str)
+            {
+               *value = *options_str;
+
+               // clear so next iteration will be new argument
+               *options_str = NULL;
+            }
+            else
+               *value = ra_advance_arg(tour);
+         }
+         return RA_SUCCESS;
+      }
+      else
+         return RA_UNKNOWN_OPTION;
+   }
+
+   return RA_END_ARGS;
+}
+
+EXPORT void ra_write_warning(FILE *f,
+                             raStatus status,
+                             const raTour *tour,
+                             const raOpt *option,
+                             const char *value)
+{
+   fprintf(f, "%s: ", ra_command_name());
+
+   if (option)
+   {
+      if (option->letter)
+         fprintf(f, "-%c: ", option->letter);
+      else if (option->label)
+         fprintf(f, "--%s: ", option->label);
+   }
+
+   switch(status)
+   {
+      case RA_SUCCESS:
+         fprintf(f, "no error\n");
+         break;
+      case RA_END_OPTIONS:
+         fprintf(f, "reached the end of the options\n");
+         break;
+      case RA_END_ARGS:
+         fprintf(f, "reached the end of the arguments\n");
+         break;
+      case RA_UNKNOWN_OPTION:
+         fprintf(f, "%s\n", *tour->current_arg);
+         break;
+      case RA_MALFORMED_OPTION:
+         fprintf(f, "option value in bad form\n");
+         break;
+      case RA_INVALID_ARGUMENT:
+         fprintf(f, "invalid argument\n");
+         break;
+      case RA_MISSING_TARGET:
+         fprintf(f, "option missing target\n");
+         break;
+      case RA_MISSING_AGENT:
+         fprintf(f, "option missing agent\n");
+         break;
+      case RA_MISSING_READER:
+         fprintf(f, "option missing reader\n");
+         break;
+      default:
+         fprintf(f, "unknown status number %d\n", (int)status);
+         break;
+   }
+}
+
+
+
+
+/* EXPORT int ra_init_cache(raCache *cache, */
+/*                          const char **start_arg, */
+/*                          int arg_count, */
+/*                          const raOpt *start_opt, */
+/*                          int opt_count) */
+/* { */
+/*    cache->args = start_arg; */
+/*    cache->args_end = start_arg + arg_count; */
+/*    cache->options = start_opt; */
+/*    cache->options_end = start_opt + opt_count; */
+
+/*    cache->current_arg = start_arg; */
+/*    cache->current_option = start_opt; */
+/*    cache->last_position_option = NULL; */
+/*    cache->sub_arg_ptr = NULL; */
+
+/*    return 1; */
+/* } */
+
+/* EXPORT const char *ra_next_arg(raCache *cache) */
+/* { */
+/*    cache->sub_arg_ptr = NULL; */
+/*    if (cache->current_arg + 1 < cache->args_end) */
+/*    { */
+/*       ++cache->current_arg; */
+/*       return *cache->current_arg; */
+/*    } */
+/*    else */
+/*       return NULL; */
+/* } */
 
 #ifdef CACHE_MAIN
 
+#include "agents.c"
+
+const char *path = NULL;
+int        number = 0;
+int        flag = 0;
+
 raOpt options[] = {
-   { 'h', "help", "This help message", NULL, NULL },
-   { 'v', "version", "Version number", NULL, NULL }
+   { 'h', "help", "This help message",  NULL,             NULL },
+   { 'v', "version", "Version number",  NULL,             NULL },
+   { 'p', "path", "Set file path",      &ra_string_agent, &path },
+   { 'n', "number", "Set number value", &ra_int_agent,    &number },
+   { 'f', "flag", "Set flag to on",     &ra_flag_agent,   &flag }
 };
+
+void display_option(const raOpt *option, const char *value, int wellformed)
+{
+   if (!wellformed)
+      printf("Malformed option: ");
+
+   if (option->letter)
+      printf("-%c", option->letter);
+   else if (option->label)
+      printf("--%s", option->label);
+
+   if (value)
+      printf(":%s\n", value);
+   else
+      printf("\n");
+}
 
 int main(int argc, const char **argv)
 {
-   raCache cache;
-   ra_init_cache(&cache, argv, argc, options, OPTS_COUNT(options));
-   g_command_name = ra_command_name(&cache);
+   raTour tour;
+   ra_set_scene(argv, argc, options, OPTS_COUNT(options));
+   ra_start_tour(&tour);
 
-   printf("Processing command \e[32;1m%s\e[m:\n", ra_command_name(&cache));
-   printf("Number of options defined: %ld.\n", cache.options_end - cache.options);
-   
    const char *arg;
    int count = 0;
-   while ((arg = ra_next_arg(&cache)))
+
+   while ((arg = ra_advance_arg(&tour)))
       printf("%2d: %s\n", ++count, arg);
+
+   const raOpt *option;
+   const char *value;
+
+   ra_start_tour(&tour);
+
+   while (1)
+   {
+      switch(ra_advance_option(&tour, &option, &value))
+      {
+         case RA_SUCCESS:
+            switch(ra_execute_option_read(option, value))
+            {
+               case RA_SUCCESS:
+                  break;
+               case RA_INVALID_ARGUMENT:
+               case RA_MISSING_AGENT:
+               case RA_MISSING_READER:
+                  break;
+               default:
+                  break;
+            }
+            break;
+         case RA_END_ARGS:
+         case RA_END_OPTIONS:
+            goto end_option_walk;
+         case RA_UNKNOWN_OPTION:
+            fprintf(stderr, "Option %s is not known.\n", *tour.current_arg);
+            break;
+         case RA_MALFORMED_OPTION:
+            display_option(option, value, 0);
+            break;
+         case RA_MISSING_TARGET:
+            fprintf(stderr, "%s: missing target for option.\n", ra_command_name());
+            break;
+         case RA_INVALID_ARGUMENT:
+            fprintf(stderr, "%s: argument value is the wrong type.\n", ra_command_name());
+            break;
+         default:
+            break;
+      }
+   }
+end_option_walk:
+
+   return 0;
 }
 
 #endif
