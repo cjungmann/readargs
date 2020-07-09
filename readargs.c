c9eec372e59b25d3461d99cbf1b6b57f24c5ba81
Chuck Jungmann <chuckj60@gmail.com>
2020-07-09 07:30:47 -0500

    Add and consolidate code to print value types with long option names in help output.

--- readargs.c
+++ readargs.c
@@ -1,6 +1,7 @@
 #include "readargs.h"
 
 #include <string.h>
+#include <alloca.h>
 
 extern raScene g_scene;
 
@@ -29,14 +30,74 @@ typedef enum _raOpt_filter
 {
    ROF_ALL = 0,
    ROF_OPTIONS,
-   ROF_ARGS
+   ROF_ARGS,
+   ROF_VALUES = 8
 } OptFilter;
 
 int include_in_option_list(OptFilter set, const raOpt *ptr)
 {
-   return set == ROF_ALL
-      || (set==ROF_OPTIONS && ra_is_named_option(ptr))
-      || (set==ROF_ARGS && ra_is_positional_option(ptr));
+   return set & ROF_ALL
+      || (set & ROF_OPTIONS && ra_is_named_option(ptr))
+      || (set & ROF_ARGS && ra_is_positional_option(ptr));
+}
+
+int process_label(const raOpt *ptr, OptFilter set, char *buffer, int len_buffer)
+{
+   int len_label;
+   int len_value;
+
+   const char *str = NULL;
+   len_label = len_value = 0;
+
+   if (set & ROF_OPTIONS)
+      str = ptr->label;
+   else if (set & ROF_ARGS)
+   {
+      if (!(str = ptr->type))
+         str = ptr->label;
+   }
+
+   if (str)
+   {
+      if (*str == '*')
+         ++str;
+
+      len_label = strlen(str);
+      if (len_label < len_buffer)
+      {
+         memcpy(buffer, str, len_label);
+         buffer += len_label;
+         len_buffer -= len_label;
+         *buffer = '\0';
+      }
+   }
+
+   if (set & ROF_VALUES && ra_is_value_option(ptr))
+   {
+      if (!(str = ptr->type))
+         str = "VALUE";
+
+      len_value = 2 + strlen(str);  // space for '=' and '\0'
+
+      if (len_value < len_buffer)
+      {
+         *buffer++ = '=';
+         memcpy(buffer, str, len_value);
+         buffer[len_value] = '\0';
+      }
+   }
+
+   return len_label + len_value;
+}
+
+int get_label_length(const raOpt *ptr, OptFilter set)
+{
+   return process_label(ptr, set, NULL, 0);
+}
+
+int set_label_value(const raOpt *ptr, OptFilter set, char *buffer, int buffer_len)
+{
+   return process_label(ptr, set, buffer, buffer_len);
 }
 
 /**
@@ -46,22 +107,16 @@ int get_max_label_length(OptFilter set)
 {
    int len_max = 0;
    int len_label;
+
    const raOpt *ptr = g_scene.options;
-   const char *str;
    while ( ptr < g_scene.options_end )
    {
       if (include_in_option_list(set, ptr))
       {
-         if ((str = ptr->label))
-         {
-            // Exclude argument prefix flag if found:
-            if (*str == '*')
-               ++str;
+         len_label = get_label_length(ptr, set);
 
-            len_label = strlen(str);
-            if (len_label > len_max)
-               len_max = len_label;
-         }
+         if (len_label > len_max)
+            len_max = len_label;
       }
       ++ptr;
    }
@@ -69,23 +124,47 @@ int get_max_label_length(OptFilter set)
    return len_max;
 }
 
-void print_option_names(FILE *f, const raOpt *opt, int max_label)
+int get_max_argument_length(void)
 {
+   int len_max = 0;
+   int len_label;
+   const raOpt *ptr = g_scene.options;
    const char *str;
+   while ( ptr < g_scene.options_end )
+   {
+      if (ptr->type)
+         str = ptr->type;
+      else if (ptr->label)
+         str = ptr->label;
+      else
+         str = "";
+
+      len_label = strlen(str);
+
+      if (len_label > len_max)
+         len_max = len_label;
+
+      ++ptr;
+   }
+
+   return len_max;
+}
+
+void print_option_names(FILE *f, const raOpt *opt, int max_label, OptFilter set)
+{
+   int len_label = get_label_length(opt, set);
+   char *str = (char*)alloca(len_label+1);
+   set_label_value(opt, set, str, len_label+1);
+
    if (opt->letter > 0)
    {
-      if (opt->label)
-         fprintf(f, "-%c, --%-*s  ", opt->letter, max_label, opt->label);
+      if (str)
+         fprintf(f, "-%c, --%-*s  ", opt->letter, max_label, str);
       else
          fprintf(f, "-%c    %-*s  ", opt->letter, max_label, "");
    }
-   else if ((str = opt->label))
-   {
-      if (*str == '*')
-         ++str;
-
-      fprintf(f, "    --%-*s  ", max_label, opt->label);
-   }
+   else if (str)
+      fprintf(f, "    --%-*s  ", max_label, str);
 }
 
 void print_option_help(FILE *f, const raOpt *opt, int max_label, int indent)
@@ -93,9 +172,24 @@ void print_option_help(FILE *f, const raOpt *opt, int max_label, int indent)
    if (indent>0)
       fprintf(f, "%*s", indent, "");
 
-   print_option_names(f, opt, max_label);
+   print_option_names(f, opt, max_label, ROF_OPTIONS | ROF_VALUES);
    fputs( opt->description, f);
    fputc('\n', f);
+}
+
+void print_argument_help(FILE *f, const raOpt *opt, int max_label, int indent)
+{
+   const char *left = NULL;
+
+   if (indent>0)
+      fprintf(f, "%*s", indent, "");
+
+   if (opt->type)
+      left = opt->type;
+   else if (opt->label)
+      left = opt->label;
+
+   fprintf(f, "%-*s  %s\n", max_label, left, opt->description);
 }
 
 int option_value_is_showable(const raOpt* option)
@@ -111,7 +205,7 @@ EXPORT void ra_show_scene_values(FILE *f)
   {
      if (option_value_is_showable(ptr))
      {
-        print_option_names(f, ptr, len_label);
+        print_option_names(f, ptr, len_label, ROF_ALL);
         ra_execute_option_write(f, ptr);
         fprintf(f, "\n");
      }
@@ -229,7 +323,7 @@ EXPORT void ra_describe_options(FILE *f, int indent)
    // Get max width of names, then
    // pass the information to an
    // option printing function.
-   int len_label = get_max_label_length(ROF_OPTIONS);
+   int len_label = get_max_label_length(ROF_OPTIONS | ROF_VALUES);
 
    const raOpt *ptr = g_scene.options;
    while ( ptr < g_scene.options_end )
@@ -247,14 +341,14 @@ EXPORT void ra_describe_arguments(FILE *f, int indent)
    // Get max width of names, then
    // pass the information to an
    // option printing function.
-   int len_label = get_max_label_length(ROF_ARGS);
+   int len_label = get_max_argument_length();
 
    const raOpt *ptr = g_scene.options;
    while ( ptr < g_scene.options_end )
    {
       if (ra_is_positional_option(ptr))
       {
-         print_option_help(f, ptr, len_label, indent);
+         print_argument_help(f, ptr, len_label, indent);
       }
       ++ptr;
    }
