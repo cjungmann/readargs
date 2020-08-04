e05031d611c35778b2b707b2c13eb39d78e86ea9
Chuck Jungmann <chuckj60@gmail.com>
2020-08-04 14:54:49 -0500

    Changed invisible raReader implementations.

--- invisible.h
+++ invisible.h
@@ -32,18 +32,18 @@ const raOpt *seek_next_positional_option(raTour *tour);
 int str_is_number(const char *str);
 int confirm_option_target(const raOpt *opt);
 /* ra_flag_agent */
-raStatus flag_reader(const raOpt *opt, const char *str);
+raStatus flag_reader(const raOpt *opt, const char *str, raTour *tour);
 void flag_writer(FILE *f, const raOpt *opt);
 /* ra_int_agent */
-raStatus int_reader(const raOpt *opt, const char *str);
+raStatus int_reader(const raOpt *opt, const char *str, raTour *tour);
 void int_writer(FILE *f, const raOpt *opt);
 /* ra_string_agent */
-raStatus string_reader(const raOpt *opt, const char *str);
+raStatus string_reader(const raOpt *opt, const char *str, raTour *tour);
 void string_writer(FILE *f, const raOpt *opt);
 /* ra_help_agent */
-raStatus show_help_reader(const raOpt *opt, const char *str);
+raStatus show_help_reader(const raOpt *opt, const char *str, raTour *tour);
 /* ra_show_values_agent */
-raStatus show_option_values_reader(const raOpt *opt, const char *value);
+raStatus show_option_values_reader(const raOpt *opt, const char *value, raTour *tour);
 
 
 
