9f2114006cbc23818fb8293f13d19bb8a7abb0c2
Chuck Jungmann <chuckj60@gmail.com>
2020-10-09 10:48:24 -0500

    code tweaks

--- samples/hello.c
+++ samples/hello.c
@@ -8,7 +8,7 @@ const char *name = "World";
 // *Note 2: Action Map definition.
 raAction actions[] = {
    { 'n', "name", "Set name",     &ra_string_agent, &name, "STRING" },
-   { 'h', "help", "This display", &ra_show_help_agent   },
+   { 'h', "help", "This display", &ra_show_help_agent   }
 };
 
 int main(int argc, const char **argv)
@@ -19,10 +19,7 @@ int main(int argc, const char **argv)
    // *Note 4: Call library to process arguments.
    if (ra_process_arguments())
    {
-      if (name)
-         printf("Hello, %s.\n", name);
-      else
-         printf("Hello, world.\n");
+      printf("Hello, %s.\n", name);
    }
    return 0;
 }
